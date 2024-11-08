ed7ac23df9fc0c23d379442a14f40ee83ce9baab
Jason Smith <122671423+JGSmith12@users.noreply.github.com>
2024-11-08 13:26:51 +0100

    add button reverse effect

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const modal = document.getElementById("modal");
 const modalCloseBtn = document.getElementById("modal-close-btn");
 const consentForm = document.getElementById("consent-form");
 const modalText = document.getElementById("modal-text");
+const declineBtn = document.getElementById("decline-btn");
+const modalChoiceBtns = document.getElementById("modal-choice-btns");
 
 setTimeout(function () {
   modal.style.display = "inline";
@@ -29,10 +31,15 @@ consentForm.addEventListener("submit", function (e) {
   setTimeout(function () {
     document.getElementById(
       "modal-inner"
-    ).innerHTML = `<h2>Thanks <span class="modal-display-name">${userName}</span> you sucker!</h2>
+    ).innerHTML = `<h2>Thanks <span class="modal-display-name">${userName}</span>, you sucker!</h2>
     <p>We just sold the rights to your eternal soul.</p>
     <div class="idiot-gif">
         <img src="images/pirate.gif">
     </div>`;
+    modalCloseBtn.disabled = false;
   }, 3000);
+});
+
+declineBtn.addEventListener("mouseover", function () {
+  modalChoiceBtns.classList.toggle("reverse");
 });
